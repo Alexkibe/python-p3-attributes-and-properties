e114cbc71c3971efcb3aed65acedf152f4c3d8b6
Alex Kibe <alex.kibe01@student.moringaschool.com>
2023-05-24 05:24:14 +0300

    Completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,31 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="", breed=""):
+        assert isinstance(name, str) and 0 < len(name) <= 25, "Name must be a string between 1 and 25 characters."
+        self.name = name.title()
+
+        assert isinstance(breed, str), "Name must be a string."
+        assert breed in APPROVED_BREEDS, "Breed must be in the list of approved jobs."
+        self.breed = breed
+    
+   
+    
+        
+        
+    
+    
+    
+    
+       
+     
+    
+
+
+   
+   
+    
+    
+    
+
+    
